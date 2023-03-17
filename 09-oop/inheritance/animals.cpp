c09a31f24687b7ab244e819af3f0da8be61dd090
bvquoc <bvquoc.dev@gmail.com>
2023-03-17 22:01:44 +0700

    animal

--- 09-oop/inheritance/animals.cpp
+++ 09-oop/inheritance/animals.cpp
@@ -24,6 +24,16 @@ protected:
         cout << "Gender: " << (this->gender == MALE ? "male" : "female") << "\n";
     }
 
+    void input() {
+        cout << "Name: ";
+        getline(cin, name);
+        cout << "Age: ";
+        cin >> age;
+        cout << "Gender (1/0): ";
+        cin >> gender;
+        cin.ignore();
+    }
+
 };
 
 class Cat : public Animal {
@@ -44,23 +54,67 @@ public:
         this->featherColor = color;
     }
 
+    void input() {
+        Animal::input();
+        cout << "Feather color: ";
+        cin >> featherColor;
+    }
     void info() { 
         Animal::info();
         cout << "Feather Color: " << this->featherColor << "\n\n";
     }
 };
 
-// class Dog: 
+class Dog:public Animal
+{
+private:
+    string featherColor;
+    string species;
+public:
+    void setAge(int age){
+        this->age=age;
+    }
+    void setName(string name){
+        this->name=name;
+    }
+    void setGender(bool gender){
+        this->gender=gender;
+    }
+    void setFeatherColor(string color){
+        this->featherColor=color;
+    }
+    void setSpecies(string species){
+        this->species=species;
+    }
+
+    void eat() {
+        cout << name << ' ';
+        Animal::eat();
+        
+    }
+    void input() {
+        Animal::input();
+        cout << "Feather color: ";
+        cin >> featherColor;
+        cout << "Species: ";
+        cin >> species;
+    }
+    void info() { 
+        Animal::info();
+        cout << "Feather Color: " << this->featherColor << endl;
+        cout << "Species: " << this->species << "\n\n";
+    }
+
+};
 
 signed main(void) {
 
-    Cat myCat;
-    myCat.setAge(2);
-    myCat.setName("My Dieu");
-    myCat.setGender(FEMALE);
-    myCat.setFeatherColor("yellow");
+    Cat a;
+    a.input();
+    a.info();
 
-    myCat.info();
-
+    Dog b;
+    b.input();
+    b.info();
     return 0;
 }
